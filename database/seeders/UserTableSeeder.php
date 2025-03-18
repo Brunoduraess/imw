0679fb2c2b28b0a35580391f224856b2d9425e18
BrunoDuraess <brunoduraes03@gmail.com>
2025-03-18 12:18:57 -0300

    fix: data to test login

---
+++ database/seeders/UserTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UserTableSeeder extends Seeder
+{
+    public function run()
+    {
+        DB::table('users')->insert([
+            'id' => '0000915b-434e-48e1-a837-048694f87d56',
+            'nome' => 'Bruno Durães de Souza Gonçalves',
+            'cpf' => '17511527744',
+            'email' => 'brunoduraes03@gmail.com',
+            'senha' => bcrypt('teste'),
+            'acesso' => 'admin',
+            'ultimo_acesso' => NULL,
+            'criado_em' => date('Y-m-d H:i:s')]
+        );
+    }
+}
