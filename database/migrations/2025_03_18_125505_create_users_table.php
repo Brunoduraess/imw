f301698ea963d3ad7014547310f18047e1df5d33
BrunoDuraess <brunoduraes03@gmail.com>
2025-03-18 12:19:16 -0300

    fear: model table users

---
+++ database/migrations/2025_03_18_125505_create_users_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->uuid('id')->primary()->autoIncrement();
+            $table->string('nome', 100)->unique();
+            $table->string('cpf', 11)->unique();
+            $table->string('email', 100)->unique();
+            $table->string('senha', 255);
+            $table->string('acesso', 50);
+            $table->dateTime('ultimo_acesso')->nullable();
+            $table->dateTime('criado_em');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
