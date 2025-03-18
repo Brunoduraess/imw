72a27be3e426c400c9eaf0c6a7d188f0cdf134d3
BrunoDuraess <brunoduraes03@gmail.com>
2025-03-18 12:17:59 -0300

    fear: authenticator controller

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function login()
+    {
+        return view('admin/login');
+    }
+
+    public function loginSubmit(Request $request)
+    {
+        $request->validate(
+            [
+                'cpf' => 'required',
+                'senha' => 'required|min:5|max:16'
+            ],
+            [
+                'cpf.required' => 'O cpf é obrigatório',
+                'senha.required' => 'A senha é obrigatória',
+                'senha.min' => 'A senha deve ter pelo menos :min caracteres',
+                'senha.max' => 'A senha deve ter no máximo :max caracteres',
+            ]
+        );
+
+        $arrayEspeciais = ['.', '-', '/'];
+        $cpf = str_replace($arrayEspeciais, '', $request->input('cpf'));
+        $senha = $request->input('senha');
+
+        $user = User::where('cpf', $cpf)->where('deleted_at', NULL)->first();
+
+        if (!$user) {
+            return redirect()->back()->withInput()->with('loginError', 'Username ou password incorretos');
+        }
+
+        if (!password_verify($senha, $user->senha)) {
+            return redirect()->back()->withInput()->with('loginError', 'Username ou password incorretos');
+        }
+
+        $user->ultimo_acesso = date('Y-m-d H:i:s');
+        $user->save();
+
+        session([
+            'user' => [
+                'id' => $user->id,
+                'nome' => $user->nome
+            ]
+        ]);
+
+        return redirect()->to('/');
+    }
+
+    public function forgot_password()
+    {
+        return view('forgot_password');
+    }
+
+}
