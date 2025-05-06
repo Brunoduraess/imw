cff0aaa29552417385ad23cb6e3792bf5c5ca130
BrunoDuraess <brunoduraes03@gmail.com>
2025-05-06 19:53:25 -0300

    fear: create models

---
+++ app/Models/Event.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Event extends Model
+{
+    protected $keyType = 'string';
+
+    public $incrementing = false;
+    const CREATED_AT = 'criado_em';
+
+    const UPDATED_AT = 'atualizado_em';
+
+    public function location(): BelongsTo
+    {
+        return $this->belongsTo(Location::class);
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'criado_por', 'id');
+    }
+}
--- app/Models/Location.php
+++ app/Models/Location.php
@@ -5,16 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
-class User extends Model
+class Location extends Model
 {
     protected $keyType = 'string';
     public $incrementing = false;
-    const CREATED_AT = 'criado_em';
-    const UPDATED_AT = 'atualizado_em';
-
     public function events(): HasMany
     {
         return $this->hasMany(Event::class);
     }
-
 }
